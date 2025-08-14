e090bf6f0a0d3f8751794e572df890d2c18c1111
superjojo140 <github@superjojo.de>
2025-08-14 11:13:21 +0200

    update backend response codes

--- src/backend/project/controller.ts
+++ src/backend/project/controller.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Request as ExpressRequest, Response as ExpressResponse} from "express";
 import * as projectService from "./service";
 
 const router = express.Router();
@@ -10,7 +10,7 @@ router.put("/:projectId", updateProject);
 router.delete("/:projectId", deleteProject);
 export default router;
 
-async function getAllProjects(req, res, next) {
+async function getAllProjects(req: ExpressRequest, res: ExpressResponse, next: NextFunction) {
     try {
         let projects = await projectService.getAllProjects();
         res.status(200).json(projects);
@@ -18,7 +18,7 @@ async function getAllProjects(req, res, next) {
     catch (e) { next(e); }
 }
 
-async function getProject(req, res, next) {
+async function getProject(req: ExpressRequest, res: ExpressResponse, next: NextFunction) {
     try {
         const projectId = req.params.projectId;
         let projectData = await projectService.getProject(projectId);
@@ -27,19 +27,19 @@ async function getProject(req, res, next) {
     catch (e) { next(e); }
 }
 
-async function createProject(req, res, next) {
+async function createProject(req: ExpressRequest, res: ExpressResponse, next: NextFunction) {
     try {
         const title = req.body.title;
         const description = req.body.description;
         const notes = req.body.notes;
 
         const projectId = await projectService.createProject(title, description, notes);
-        res.status(201).json({ status: "success", message: "Created project successfully", projectId: projectId });
+        res.status(200).json({ status: "success", message: "Created project successfully", projectId: projectId });
     }
     catch (e) { next(e); }
 }
 
-async function updateProject(req, res, next) {
+async function updateProject(req: ExpressRequest, res: ExpressResponse, next: NextFunction) {
     try {
         const title = req.body.title;
         const description = req.body.description;
@@ -47,17 +47,17 @@ async function updateProject(req, res, next) {
         const projectId = req.params.projectId;
 
         await projectService.updateProject(projectId, title, description, notes);
-        res.status(204).json({ status: "success", message: "Updated project successfully" });
+        res.status(200).json({ status: "success", message: "Updated project successfully" });
     }
     catch (e) { next(e); }
 }
 
-async function deleteProject(req, res, next) {
+async function deleteProject(req: ExpressRequest, res: ExpressResponse, next: NextFunction) {
     try {
         const projectId = req.params.projectId;
 
         await projectService.deleteProject(projectId);
-        res.status(204).json({ status: "success", message: "Deleted project successfully" });
+        res.status(200).json({ status: "success", message: "Deleted project successfully" });
     }
     catch (e) { next(e); }
 }
